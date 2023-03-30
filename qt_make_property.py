2c5fcc2f97c71464f8e36c3cfda0ad713eca0b82
Pieter de Goeje <pieter.de.goeje@demcon.com>
2023-03-30 16:52:55 +0200

    Deal with pointers to QObjects

--- qt_make_property.py
+++ qt_make_property.py
@@ -10,6 +10,7 @@ properties = []
 variables = []
 functions = []
 signals = []
+slots = []
 
 for line in sys.argv[1].splitlines():
     decl = line.strip()
@@ -21,20 +22,52 @@ for line in sys.argv[1].splitlines():
         sys.exit(1)
         
     m_type = match.group(1).strip()
+
+    m_isPointer = m_type.endswith("*")
+
     m_name = match.group(3)
     m_Name = m_name[0].upper() + m_name[1:]
     m_notify = f"{m_name}Changed"
     m_var = "m_" + m_name
+    m_destroyed = f"{m_name}DestroyedHandler"
 
     properties.append(f"Q_PROPERTY({m_type} {m_name} READ {m_name} WRITE set{m_Name} NOTIFY {m_notify})")
     variables.append(f"{m_type} m_{m_name} {{}};")
     functions.append(f"{m_type} {m_name}() const {{ return {m_var}; }}")
-    functions.append(f"void set{m_Name}({m_type} v) {{ if(v != {m_var}) {{ {m_var} = v; emit {m_notify}(); }} }}")
     signals.append(f"void {m_notify}();")
+    if m_isPointer:
+        functions.append(f"""void set{m_Name}({m_type} v) {{
+    if(v == {m_var}) {{
+      return;
+    }}
+    if({m_var} != nullptr) {{
+      /* Add (optional) code to deref {m_var} here */
+      disconnect({m_var}, &QObject::destroyed, this, &CLASS_NAME::{m_destroyed});
+    }}
+    {m_var} = v;
+    if({m_var} != nullptr) {{
+      connect({m_var}, &QObject::destroyed, this, &CLASS_NAME::{m_destroyed});
+    }}
+    emit {m_notify}();
+  }}"""
+        )
+        slots.append(f"""void {m_destroyed}(QObject *o) {{
+    Q_ASSERT({m_var} == o);
+    /* Add (optional) code to deref {m_var} here */
+    {m_var} = nullptr;
+    emit {m_notify}();
+  }}""")
+    else:
+        functions.append(f"void set{m_Name}({m_type} v) {{ if(v != {m_var}) {{ {m_var} = v; emit {m_notify}(); }} }}")
+
+
 
 print("\n".join(["  " + s for s in properties]))
 print("\n".join(["  " + s for s in variables]))
 print("public:")
 print("\n".join(["  " + s for s in functions]))
+if len(slots):
+    print("private slots:")
+    print("\n".join(["  " + s for s in slots]))
 print("signals:")
 print("\n".join(["  " + s for s in signals]))
